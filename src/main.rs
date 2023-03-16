fc456044bcac8896f1e8bdf61f3838e32aff5365
Doc Jones <doc.jones.atwork@gmail.com>
2023-03-16 19:00:29 -0400

    Refactor to separate get_prs and main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
+
+use reqwest::Error;
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use github_merged_prs_lib::fetch_merged_prs;
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), Error> {
+    let repo_owner = "graphql";
+    let access_token = env::var("GITHUB_ACCESS_TOKEN").expect("Missing GITHUB_ACCESS_TOKEN environment variable");
+
+    let client = reqwest::Client::new();
+
+    let repos = vec![
+        "graphql-js",
+        "graphql-spec",
+        "graphiql",
+        // Add more repository names as needed
+    ];
+
+    //let mut output_file = File::create("merged_prs.txt")?;
+
+    for repo_name in repos {
+        fetch_merged_prs(&repo_owner, &repo_name, &access_token, &client, &mut output_file).await?;
+    }
+
+    Ok(())
 }
+
